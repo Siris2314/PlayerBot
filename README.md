78e43498116ff0ade57b8e41e382304ac60be021
Atlinx <atlinuxtm@gmail.com>
2021-10-15 19:28:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fallen Atomic Bot (WIP)
+
+A Discord bot that you can play games with!
---
+++ constants.js
@@ -0,0 +1,18 @@
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+
+module.exports = {
+	defaultEmbed(type) {
+		const embed = new MessageEmbed()
+			.setColor('#742f67')
+			.setTimestamp()
+			.setFooter('Fallen Atomic Bot', 'https://cdn.discordapp.com/app-icons/426554007977197579/3fd22dff0bf6f9b65dfe945cc25ee24b.png');
+		if (typeof type === 'string') {
+			switch (type.toUpperCase()) {
+				case 'ATTENTION':
+					embed.setTitle('‼️ Attention')
+					break;
+			}
+		}
+		return embed;
+	}
+}
