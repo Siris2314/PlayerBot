35559fd93d6785900791a9f6427a545472e69242
Atlinx <atlinuxtm@gmail.com>
2021-10-16 15:50:21 -0400

    Update theme for embeds

--- constants.js
+++ constants.js
@@ -3,9 +3,9 @@ const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 module.exports = {
 	defaultEmbed(type) {
 		const embed = new MessageEmbed()
-			.setColor('#742f67')
+			.setColor('#52A42B')
 			.setTimestamp()
-			.setFooter('Player2', 'https://cdn.discordapp.com/app-icons/426554007977197579/3fd22dff0bf6f9b65dfe945cc25ee24b.png');
+			.setFooter('Player2', 'https://cdn.discordapp.com/avatars/898684090021212272/fb9b735c7ca100d1b573512be5685925.png');
 		if (typeof type === 'string') {
 			switch (type.toUpperCase()) {
 				case 'ATTENTION':
